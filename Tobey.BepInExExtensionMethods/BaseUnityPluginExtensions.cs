3fb98afc382d41b60f457f5dfdd18c08ae870012
Tobey Blaber <contact@tobey.me>
2022-12-14 05:06:49 +0000

    Fall back to the BepInEx_Manager GameObject when appropriate

--- Tobey.BepInExExtensionMethods/BaseUnityPluginExtensions.cs
+++ Tobey.BepInExExtensionMethods/BaseUnityPluginExtensions.cs
@@ -4,9 +4,20 @@ namespace BepInEx
 {
     internal static class BaseUnityPluginExtensions
     {
-        public static BaseUnityPlugin GetPlugin(this BaseUnityPlugin plugin, string guid) =>
-            plugin
+        public static BaseUnityPlugin GetPlugin(this BaseUnityPlugin plugin, string guid)
+        {
+            var result = plugin
                 .GetComponents<BaseUnityPlugin>()
                 .FirstOrDefault(p => p.Info.Metadata.GUID == guid);
+
+            if (result == null && plugin.gameObject != Bootstrap.Chainloader.ManagerObject)
+            {
+                result = Bootstrap.Chainloader.ManagerObject
+                    .GetComponents<BaseUnityPlugin>()
+                    .FirstOrDefault(p => p.Info.Metadata.GUID == guid);
+            }
+
+            return result;
+        }
     }
 }
