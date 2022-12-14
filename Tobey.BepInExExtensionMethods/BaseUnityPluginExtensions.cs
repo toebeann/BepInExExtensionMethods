cab7d88a4ab8fefa5ed2da726df6e986bf3a5765
Tobey Blaber <contact@tobey.me>
2022-12-14 04:16:12 +0000

    Initial commit

---
+++ Tobey.BepInExExtensionMethods/BaseUnityPluginExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace BepInEx
+{
+    internal static class BaseUnityPluginExtensions
+    {
+        public static BaseUnityPlugin GetPlugin(this BaseUnityPlugin plugin, string guid) =>
+            plugin
+                .GetComponents<BaseUnityPlugin>()
+                .FirstOrDefault(p => p.Info.Metadata.GUID == guid);
+    }
+}
